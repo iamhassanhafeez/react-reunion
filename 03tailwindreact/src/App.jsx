601eeef9cc209f85fa8f6fa3ca64a2885051ddfa
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-04-19 14:12:17 +0500

    playing with props

--- 03tailwindreact/src/App.jsx
+++ 03tailwindreact/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
+  let myObj = {
+    name: "Hassan hafeez",
+    email: "iamhassanhafeez@gmail.com",
+  };
 
   return (
     <>
       <h1 className="bg-green-400">Vite + React</h1>
+      <Card dev="hassanHafeez" soneVal={myObj} />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
