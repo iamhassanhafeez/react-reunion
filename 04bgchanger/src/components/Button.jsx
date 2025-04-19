a760512fbdc8e4542ded130f564f0bdec5eb51c1
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-04-19 17:28:08 +0500

    cursor pointer

--- 04bgchanger/src/components/Button.jsx
+++ 04bgchanger/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ colorName, colorValue, onClick }) {
   return (
     <button
-      className={`${colorValue} capitalize px-10 py-5 outline-none rounded-full cursor-auto transition-all duration-300 ease-in-out 
+      className={`${colorValue} capitalize px-10 py-5 outline-none rounded-full cursor-pointer transition-all duration-300 ease-in-out 
                  hover:scale-105`}
       onClick={onClick}
     >
