155b4c5dda832ed52c88eaefde1bd4d36326c1ca
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-04-27 15:44:14 +0500

    added logic

--- 05passwordgen/src/App.jsx
+++ 05passwordgen/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordGenerator = () => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()-_=+[]{}`~";
+    for (let i = 1; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  };
+
+  // useEffect(() => {});
   return (
     <>
       <div className=" w-full h-screen flex flex-wrap justify-center items-center">
@@ -28,8 +43,11 @@ function App() {
                 min={6}
                 max={100}
                 className="cursor-pointer"
+                onChange={(e) => {
+                  setLength(e.target.value);
+                }}
               />
-              <label>Length: </label>
+              <label>Length:{length} </label>
             </div>
             <div className="flex items-center gap-x-1">
               <input
